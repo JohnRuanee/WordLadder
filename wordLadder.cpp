8a23c94e2e38f279dbb8d76a24a2aa038e8f86ec
JohnRuanee <65741799+JohnRuanee@users.noreply.github.com>
2022-12-13 17:43:30 -0500

    did stuff

--- wordLadder.cpp
+++ wordLadder.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <unordered_map>
 #include <queue>
+#include <map>
 using namespace std;
 
 vector<string> V; //words from wordlist05.txt
@@ -11,9 +12,80 @@ vector<string> V; //words from wordlist05.txt
 
 //Implement breadth-first search, refer to Lab 10
 
-void wordLadder(string s, string t, int &steps, vector<string> &p)
-{
-  //Implement this function
+map<string, bool> visited;         // have we queued up this state for visitation?
+map<string, string> pred;           // predecessor state we came from
+map<string, int> dist;             // distance (# of hops) from source node
+map<string, vector<string> > strs;   // vector of neighboring states
+
+map<pair<string,string>, string> edge_label;
+
+// GENERIC (breadth-first search, outward from curnode)
+void search(string source_node){
+  queue<string> to_visit;
+  to_visit.push(source_node);
+  visited[source_node] = true;
+  dist[source_node] = 0;
+  
+  while (!to_visit.empty()) {
+    string curnode = to_visit.front();
+    to_visit.pop();
+    for (string n : strs[curnode])
+      if (!visited[n]) {
+        pred[n] = curnode;
+        dist[n] = 1 + dist[curnode];
+        visited[n] = true;
+        to_visit.push(n);
+      }
+  }
+}
+
+void buildGraph(){
+  ifstream file("wordlist05.txt");
+  string word;
+
+  while(getline(file, word)){
+    V.push_back(word);
+    strs[word] = vector<string>();
+  }
+
+  for(string w: V){
+    for(int i = 0; i < 5; i++){
+      for(int a = 97; a < 97 + 26; a++){
+        if(w[i] == a){
+          continue;
+        }
+
+        string curr = w;
+        curr[i] = (char)a;
+
+        if(strs.find(w) != strs.end()){
+            strs[w].push_back(curr);
+        }
+      }
+    }
+  }
+}
+
+int findSteps(string s, string t, vector<string> &p){
+  int steps = 0;
+  if(pred.find(t) == pred.end()){
+    p.push_back(t);
+    return 0;
+  }else if(t != s){
+    steps = 1 + findSteps(s, pred[t], p);
+    p.push_back(t);
+    return steps;
+  }else{
+    p.push_back(t);
+    return steps;
+  }
+}
+
+void wordLadder(string s, string t, int &steps, vector<string> &p){
+  buildGraph();
+  search(s);
+  steps = findSteps(s, t, p);
+  reverse(p.begin(), p.end());
 }
 
 int main(void)
